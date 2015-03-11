a2031335119425d3fb82da84af0de3d805a21d21
Tongtao Charles Zhang <charlesztt@gmail.com>
2015-03-10 21:47:21 -0400

    20150310

--- src/com/prinzkarl/yuubari/access_sqlite/StringOperation.java
+++ src/com/prinzkarl/yuubari/access_sqlite/StringOperation.java
@@ -50,6 +50,24 @@ public class StringOperation {
 		c.close();
 		return target_value;
 	}
+	
+	public static boolean does_not_exist(String target_column, String index_id_key,
+			String index_id_value, String db_file, String table_name)
+			throws SQLException {
+		Connection c = null;
+		Statement stmt = null;
+		c = DriverManager.getConnection("jdbc:sqlite:" + db_file);
+		c.setAutoCommit(false);
+		stmt = c.createStatement();
+		String sql = "SELECT " + target_column + " FROM " + table_name
+				+ " WHERE " + index_id_key + "='" + index_id_value + "';";
+		// System.out.println(sql);
+		ResultSet rs = stmt.executeQuery(sql);
+		boolean target_value = rs.isAfterLast();
+		stmt.close();
+		c.close();
+		return target_value;
+	}
 
 	public static void update(String target_column, String new_value,
 			String index_id_key, String index_id_value, String db_file,
@@ -85,17 +103,20 @@ public class StringOperation {
 
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
+		// just tests
 		String[] columns = { "userID", "maxID", "LastID" };
 		String[] values = { "110", "001", "011" };
 		insert(columns, values, "test.db", "tweetInfo");
 		System.out.println(select("maxID", "userID", "110", "test.db",
+				"tweetInfo"));
+		System.out.println(does_not_exist("maxID", "userID", "110", "test.db",
 				"tweetInfo"));
 		update("maxID", "2333222", "userID", "110", "test.db","tweetInfo");
 		System.out.println(select("maxID", "userID", "110", "test.db",
 				"tweetInfo"));
 		delete("userID", "110", "test.db", "tweetInfo");
 		try{
-			System.out.println(select("maxID", "userID", "110", "test.db",
+			System.out.println(does_not_exist("maxID", "userID", "110", "test.db",
 					"tweetInfo"));
 		}
 		catch(SQLException e){
