8aac6ca100533d4798cda09799f1fcfe529d336f
Tongtao Charles Zhang <varanium@varanium.net>
2015-05-29 16:17:25 -0400

    merge the test codes into main

--- src/com/prinzkarl/yuubari/access_sqlite/StringOperation.java
+++ src/com/prinzkarl/yuubari/access_sqlite/StringOperation.java
@@ -107,6 +107,29 @@ public class StringOperation {
 		c.close();
 	}
 
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		
+			// This is only a test.
+			String[] columns = { "userID", "maxID", "LastID" };
+			String[] values = { "110", "001", "011" };
+			StringOperation.insert(columns, values, "test.db", "tweetInfo");
+			System.out.println(StringOperation.select("maxID", "userID", "110", "test.db",
+					"tweetInfo"));
+			System.out.println(StringOperation.does_not_exist("maxID", "userID", "110", "test.db",
+					"tweetInfo"));
+			StringOperation.update("maxID", "2333222", "userID", "110", "test.db", "tweetInfo");
+			System.out.println(StringOperation.select("maxID", "userID", "110", "test.db",
+					"tweetInfo"));
+			StringOperation.delete("userID", "110", "test.db", "tweetInfo");
+			try {
+				System.out.println(StringOperation.does_not_exist("maxID", "userID", "110",
+						"test.db", "tweetInfo"));
+			} catch (SQLException e) {
+				System.err.println("Failed");
+			}
+		
+	}
 
 
 }
